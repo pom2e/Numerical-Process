178803ee71d464866fd25fff550bc2b30da5d800
Ryo EGAMI <ryo_ega@Ryo-no-MacBook-Air.local>
2013-09-02 15:01:14 +0900

    Initial Commit

---
+++ Numerical Process/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Numerical Process
+//
+//  Created by Ryo EGAMI on 2013/09/02.
+//  Copyright (c) 2013年 Ryo EGAMI. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
